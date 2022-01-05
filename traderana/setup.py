7eb4970c9a9d7134d8b2aca760929a9b36b1e9a3
H88trader <boruotrading@gmail.com>
2022-01-05 14:10:22 -0500

    Setup for github

---
+++ traderana/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='traderana',
+    version='0.0.1',
+    author='H88trader',
+    author_email='boruotrading@gmail.com',
+    description='A Package that tracking trades',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/H88trader/traderana',
+    project_urls = {
+        "Bug Tracker": "https://github.com/H88trader/traderana/issues"
+    },
+    license='GNU GENERAL PUBLIC LICENSE v3.0',
+    packages=['traderana'],
+    install_requires=[
+                       'cycler',
+                       'et-xmlfile',
+                       'fonttools',
+                       'kiwisolver',
+                       'matplotlib',
+                       'numpy',
+                       'openpyxl',
+                       'packaging',
+                       'pandas',
+                       'Pillow',
+                       'pyparsing',
+                       'python-dateutil',
+                       'pytz',
+                       'six',
+                     ]
+)
